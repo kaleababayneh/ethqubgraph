0ad9373becc54930cf49fd6ddb80f242a80360d6
itu-itis21-gizaw21 <gizaw21@itu.edu.tr>
2025-02-27 17:21:09 +0300

    create group

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,16 +5,6 @@ import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
 
-
-import React, { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { CirclesSDK } from "../contexts/CirclesSDK";
-import { Profile } from '@circles-sdk/profiles';
-
-
-// import Dashboard from "./circles-components/dashboard";
-// import CirclesOnboarding from "./circles-components/circlesonboarding";
-
 import { Climate_Crisis} from "next/font/google"
 import { Anek_Bangla } from "next/font/google";
 
@@ -29,79 +19,18 @@ const AnekBangla = Anek_Bangla({
   subsets: ["latin"],
 });
 
-// export const metadata = getMetadata({ title: " ETHqub", description: " ETHqub: Community Saving & Lending",  });
-// TEST
-
-import { CirclesConfig, Sdk } from '@circles-sdk/sdk';
-import {BrowserProviderContractRunner} from "@circles-sdk/adapter-ethers"
+//export const metadata = getMetadata({ title: " ETHqub", description: " ETHqub: Community Saving & Lending",  });
 
 
 
-export const circlesConfig: CirclesConfig = {
-  circlesRpcUrl: "https://static.94.138.251.148.clients.your-server.de/rpc/",
-  v1HubAddress: "0x29b9a7fbb8995b2423a71cc17cf9810798f6c543",
-  v2HubAddress: "0x3D61f0A272eC69d65F5CFF097212079aaFDe8267",
-  migrationAddress: "0x28141b6743c8569Ad8B20Ac09046Ba26F9Fb1c90",
-  nameRegistryAddress: "0x8D1BEBbf5b8DFCef0F7E2039e4106A76Cb66f968",
-  baseGroupMintPolicy: "0x79Cbc9C7077dF161b92a745345A6Ade3fC626A60",
-  profileServiceUrl: "https://static.94.138.251.148.clients.your-server.de/profiles/",
-};
-
-
-export let cropWidth: number = 256;
-export let cropHeight: number = 256;
-export let imageDataUrl: string | undefined;
- 
-
-const WalletAddress = "0x7a30d670ebEb9620E35bC6034AeE69c4Fa5BC50B";
-const IpfsAddress = "bafkreib7ad4v3uij2fych73wgu2r3zwhosjbzrjipxaehcr7ispu7b7hdu";
-const Address2 =  "0xb2c687872791f1f39e2b9e52508a7b6963ff1d7b";
-
-
-
-const newProfile: Profile = {
-  name: "Kaleab Avatar Name",
-  description: "Kaleab Updated description for the avatar.",
-  imageUrl: "ipfs://QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE",
-  // previewImageUrl : "ipfs://QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE",
-};
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CirclesSDK } from "../contexts/CirclesSDK";
 
 
 
 const ScaffoldEthApp =  ({ children }: { children: React.ReactNode }) => {
 
-    React.useEffect(() => {
-      const fetchAvatar = async () => {
-        const adapter = new BrowserProviderContractRunner();
-        await adapter.init();
-
-        const sdk = new Sdk(adapter, circlesConfig);
-
-        let avatar = await sdk.getAvatar(WalletAddress);
-
-        // avatar.updateProfile(newProfile);
-
-        // console.log("Avatar 12345:", avatar);
-
-        // const isTrusted = await avatar.trusts(Address2);
-
-       // avatar.
-
-        // console.log(isTrusted); 
-        const receipt = await avatar.updateProfile(newProfile);
-        console.log("Metadata updated successfully:", receipt);
-        console.log("Avatar 678910:", avatar);
-
-
- 
-      };
-      fetchAvatar();
-    }, []);
-
-
-
-
-  //const avatarInfo =  circlesData.getAvatarInfo("0xAvatarAddress");
   return (
     <html suppressHydrationWarning>
       <body className={`${ClimateCrisis.variable} ${AnekBangla.variable} dark`}>
@@ -147,10 +76,5 @@ export default ScaffoldEthApp;
         </CirclesSDK>
       );
     }
-
-  
   */}
 
-function dispatch(arg0: string, arg1: { dataUrl: string; }) {
-  throw new Error("Function not implemented.");
-}
--- packages/nextjs/app/profile/page.tsx
+++ packages/nextjs/app/profile/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react'
+import React, { useState } from "react";
 import { useAccount } from 'wagmi';
 import Header from '~~/components/custom/Header';
 import JoinTopHeader from '~~/components/custom/JoinTopHeader';
@@ -12,25 +12,102 @@ import { useActiveAccount, useWalletBalance } from "thirdweb/react";
 import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 import WalletProfileActive from '~~/components/custom/walletProfileActive';
 import WalletProfilePast from '~~/components/custom/walletProfilePast';
-import { useState } from 'react';
+
+
+import { CirclesConfig, Sdk } from '@circles-sdk/sdk';
+import {BrowserProviderContractRunner} from "@circles-sdk/adapter-ethers"
+import { GroupProfile, Profile, Profiles } from '@circles-sdk/profiles';
 
 
 
+export const circlesConfig: CirclesConfig = {
+  circlesRpcUrl: "https://static.94.138.251.148.clients.your-server.de/rpc/",
+  v1HubAddress: "0x29b9a7fbb8995b2423a71cc17cf9810798f6c543",
+  v2HubAddress: "0x3D61f0A272eC69d65F5CFF097212079aaFDe8267",
+  migrationAddress: "0x28141b6743c8569Ad8B20Ac09046Ba26F9Fb1c90",
+  nameRegistryAddress: "0x8D1BEBbf5b8DFCef0F7E2039e4106A76Cb66f968",
+  baseGroupMintPolicy: "0x79Cbc9C7077dF161b92a745345A6Ade3fC626A60",
+  profileServiceUrl: "https://static.94.138.251.148.clients.your-server.de/profiles/",
+};
 
 
-const Profile = () => {
+ 
+
+const WalletAddress = "0x7a30d670ebEb9620E35bC6034AeE69c4Fa5BC50B";
+const Address2 =  "0xb2c687872791f1f39e2b9e52508a7b6963ff1d7b";
+
+
+
+const ProfileP = () => {
 
   const [trustRelations, setTrustRelations] = useState([]);
   
   let activeAccount = useActiveAccount();
-  //let { address: connectedAddress } = useAccount();
-  let connectedAddress = activeAccount?.address;
+  let { address: connectedAddress } = useAccount();
+  //let connectedAddress = activeAccount?.address;
+
+
+  
 
 
   useEffect(() => {
-      console.log(connectedAddress);
+    const fetchAvatar = async () => {
+      const adapter = new BrowserProviderContractRunner();
+      await adapter.init();
 
-  }, [connectedAddress]);
+      const sdk = new Sdk(adapter, circlesConfig);
+
+      let avatar = await sdk.getAvatar(WalletAddress);
+
+
+      const inviteeAddress = "0xfc2a63e79f57655aed2ca6a4e15ddc6a3644b566";
+
+      //const mintableToken = await avatar.getMintableAmount();
+      //console.log(await avatar.getProfile());
+      //const mintTransaction = await avatar.personalMint();
+      //const trustReceipt = await avatar.trust(inviteeAddress);
+      
+     avatar.getTrustRelations().then((trustRelations) => {
+      console.log('Trust relations:', trustRelations);
+      // trustRelations.forEach((relation) => { 
+      //     console.log(`${relation.objectAvatar}`);
+      // });
+     });
+
+
+     const totalBalance = await avatar.getTotalBalance();
+     console.log(`Total Circles balance: ${totalBalance}`);
+
+
+     const tokenBalances = await avatar.getBalances();
+     console.log('Token balances:', tokenBalances);
+      // tokenBalances.forEach((balance) => {
+      //   console.log(`Token: ${balance}`);
+      // });
+
+      const maxTransferable = await avatar.getMaxTransferableAmount(inviteeAddress)
+      console.log(`Maximum transferable amount: ${maxTransferable}`);
+      //console.log(trustReceipt);
+      //console.log('Transaction successful, receipt:', mintTransaction);
+
+
+      const standardMintPolicy = "0x5Ea08c967C69255d82a4d26e36823a720E7D0317";
+      const mintPolicy = standardMintPolicy
+      const groupProfile: GroupProfile = {
+          name: 'Kal',
+          symbol: 'Kal',
+          description: 'Kale'
+      };
+      console.log('Creating new group avatar...', groupProfile);
+      const GroupAvatar = await sdk.registerGroupV2(mintPolicy, groupProfile);
+
+      // Log the newly created group avatar details
+      console.log('New Group Avatar:', GroupAvatar);
+    };
+    fetchAvatar();
+  }, []);
+
+
 
 
    const { data: equbList } = useScaffoldReadContract({
@@ -63,10 +140,10 @@ const Profile = () => {
         </div>
 
         <div className='profile-content'>
-         <div className='profile-content-sidebar'>
-            <div className='profile-content-sidebar-address'>
+          <div className='profile-content-sidebar'>
+          {/* <div className='profile-content-sidebar-address'>
             {connectedAddress?.slice(0, 6)}... {connectedAddress?.slice(-6)}
-            </div>
+            </div> */}
 
             {/* <div className='profile-content-sidebar-credit'>
               <div className='profile-content-sidebar-credit-title'>
@@ -83,7 +160,7 @@ const Profile = () => {
 
           </div>
 
-          <div className='profile-content-main'>
+          {/* <div className='profile-content-main'>
             <div className='profile-content-main-active'>
                 <div className="profile-content-main-active-title">
                     Active Equbs  
@@ -143,10 +220,20 @@ const Profile = () => {
 
             </div>
        
-          </div>
+          </div> */}
         </div>
     </div>
   )
 }
 
-export default Profile;
+export default ProfileP;
+
+
+
+
+// const newProfile: Profile = {
+//   name: "Kaleab Avatar Name",
+//   description: "Kaleab Updated description for the avatar.",
+//   imageUrl: "ipfs://QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE",
+//   // previewImageUrl : "ipfs://QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE",
+// };
