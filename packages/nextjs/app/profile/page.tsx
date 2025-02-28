274d66a8de4ccfd9fb4403314d20e895a91ed213
itu-itis21-gizaw21 <gizaw21@itu.edu.tr>
2025-02-28 11:04:13 +0300

    merge stash

--- packages/nextjs/app/profile/page.tsx
+++ packages/nextjs/app/profile/page.tsx
@@ -12,8 +12,6 @@ import { useActiveAccount, useWalletBalance } from "thirdweb/react";
 import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 import WalletProfileActive from '~~/components/custom/walletProfileActive';
 import WalletProfilePast from '~~/components/custom/walletProfilePast';
-
-
 import { CirclesConfig, Sdk } from '@circles-sdk/sdk';
 import {BrowserProviderContractRunner} from "@circles-sdk/adapter-ethers"
 import { GroupProfile, Profile, Profiles } from '@circles-sdk/profiles';
@@ -40,74 +38,68 @@ const Address2 =  "0xb2c687872791f1f39e2b9e52508a7b6963ff1d7b";
 
 const ProfileP = () => {
 
-  const [trustRelations, setTrustRelations] = useState([]);
-  
+  const [totalBalance, setTotalBalance] = useState(0);
+  const [maxTransferable, setMaxTransferable] = useState(0);
+  const [mintableToken, setMintableToken] = useState(0);
+
   let activeAccount = useActiveAccount();
-  let { address: connectedAddress } = useAccount();
-  //let connectedAddress = activeAccount?.address;
-
-
+  //let { address: connectedAddress } = useAccount();
+  let connectedAddress = activeAccount?.address as `0x${string}` || "0x0000000000000000000000000000000000000000";
   
-
 
   useEffect(() => {
     const fetchAvatar = async () => {
+
+      if (!connectedAddress) return;
+
       const adapter = new BrowserProviderContractRunner();
       await adapter.init();
-
-      const sdk = new Sdk(adapter, circlesConfig);
-
-      let avatar = await sdk.getAvatar(WalletAddress);
-
-
-      const inviteeAddress = "0xfc2a63e79f57655aed2ca6a4e15ddc6a3644b566";
-
-      //const mintableToken = await avatar.getMintableAmount();
-      //console.log(await avatar.getProfile());
-      //const mintTransaction = await avatar.personalMint();
-      //const trustReceipt = await avatar.trust(inviteeAddress);
       
-     avatar.getTrustRelations().then((trustRelations) => {
-      console.log('Trust relations:', trustRelations);
+      // Force Circles SDK to use the Gnosis RPC instead of MetaMask
+      const sdk = new Sdk(adapter,  circlesConfig);// { ...circlesConfig, circlesRpcUrl: "https://rpc.gnosischain.com" });
+      let avatar = await sdk.getAvatar(connectedAddress);
+
+
+      const mintableToken = await avatar.getMintableAmount();
+      setMintableToken(mintableToken);
+
+      const balanceToken = await avatar.getTotalBalance();
+      setTotalBalance(balanceToken);
+ 
+      //const trustReceipt = await avatar.trust(inviteeAddress);
+    //  avatar.getTrustRelations().then((trustRelations) => {
+    //   console.log('Trust relations:', trustRelations);
       // trustRelations.forEach((relation) => { 
       //     console.log(`${relation.objectAvatar}`);
       // });
-     });
+     //});
 
-
-     const totalBalance = await avatar.getTotalBalance();
-     console.log(`Total Circles balance: ${totalBalance}`);
-
-
-     const tokenBalances = await avatar.getBalances();
-     console.log('Token balances:', tokenBalances);
-      // tokenBalances.forEach((balance) => {
-      //   console.log(`Token: ${balance}`);
-      // });
-
-      const maxTransferable = await avatar.getMaxTransferableAmount(inviteeAddress)
-      console.log(`Maximum transferable amount: ${maxTransferable}`);
-      //console.log(trustReceipt);
-      //console.log('Transaction successful, receipt:', mintTransaction);
-
-
-      const standardMintPolicy = "0x5Ea08c967C69255d82a4d26e36823a720E7D0317";
-      const mintPolicy = standardMintPolicy
-      const groupProfile: GroupProfile = {
-          name: 'Kal',
-          symbol: 'Kal',
-          description: 'Kale'
-      };
-      console.log('Creating new group avatar...', groupProfile);
-      const GroupAvatar = await sdk.registerGroupV2(mintPolicy, groupProfile);
-
-      // Log the newly created group avatar details
-      console.log('New Group Avatar:', GroupAvatar);
     };
     fetchAvatar();
-  }, []);
+  }, [connectedAddress]);
 
 
+  const handleMint = async () => {
+    try {
+      const adapter = new BrowserProviderContractRunner();
+      await adapter.init();
+      const sdk = new Sdk(adapter, circlesConfig);
+      let avatar = await sdk?.getAvatar(connectedAddress);
+      const mintTransaction = await avatar.personalMint();
+      setMintTransaction(mintTransaction);
+
+      const mintableToken = await avatar.getMintableAmount();
+      setMintableToken(mintableToken);
+
+      const balanceToken = await avatar.getTotalBalance();
+      setTotalBalance(balanceToken);
+
+      console.log('Mint transaction:', mintTransaction);
+
+    } catch (error) {
+      console.error('Error minting tokens:', error);
+    }
+  };
 
 
    const { data: equbList } = useScaffoldReadContract({
@@ -134,6 +126,7 @@ const ProfileP = () => {
         </div>
 
         <div className='profile-banner'>
+          <div>SHOW Circle</div>
           <div className='profile-banner-avator'>
           {connectedAddress && <BlockieAvatar address={connectedAddress} size={130}/>}
           </div>
@@ -141,9 +134,19 @@ const ProfileP = () => {
 
         <div className='profile-content'>
           <div className='profile-content-sidebar'>
-          {/* <div className='profile-content-sidebar-address'>
-            {connectedAddress?.slice(0, 6)}... {connectedAddress?.slice(-6)}
-            </div> */}
+
+            <div className='profile-content-sidebar-address'>
+              {connectedAddress?.slice(0, 6)}... {connectedAddress?.slice(-6)}
+            </div>
+
+            <h1>
+              Mintable Token: {mintableToken.toFixed(2)} CRC
+            </h1>
+
+            <h1>
+              Total Balance: {totalBalance.toFixed(2)} CRC
+            </h1>
+
 
             {/* <div className='profile-content-sidebar-credit'>
               <div className='profile-content-sidebar-credit-title'>
@@ -155,13 +158,14 @@ const ProfileP = () => {
                 <div className='profile-content-sidebar-credit-score-value'>
                   70
                 </div>
-              </div>
-            </div> */}
+              </div> */}
+
+            </div> 
 
           </div>
 
-          {/* <div className='profile-content-main'>
-            <div className='profile-content-main-active'>
+          <div className='profile-content-main'>
+            {/* <div className='profile-content-main-active'>
                 <div className="profile-content-main-active-title">
                     Active Equbs  
                 </div>
@@ -218,9 +222,9 @@ const ProfileP = () => {
                   </div>
                 </div>
 
-            </div>
+            </div> */}
        
-          </div> */}
+          </div>
         </div>
     </div>
   )
--- packages/nextjs/components/custom/Header.tsx
+++ packages/nextjs/components/custom/Header.tsx
@@ -16,10 +16,12 @@ const liskSepolia =  defineChain({
         rpc: "https://rpc.sepolia-api.lisk.com"
 });
 
+
 const gnosisChain =  defineChain({
     id : 100,
    rpc: "https://gnosis.drpc.org"
 });
+
 
 const sepolia =  defineChain({
   id: 11155111,
@@ -32,9 +34,10 @@ const Header = () => {
 
       
       let activeAccount = useActiveAccount();
-      let { address: connectedAddress } = useAccount();
-    
-      //let connectedAddress = activeAccount?.address;
+      //let { address: connectedAddress } = useAccount();
+      let connectedAddress = activeAccount?.address;
+      //let { address: connectedAddress } = useAccount();
+      let connectedAddress = activeAccount?.address;
       
 
 
@@ -64,8 +67,8 @@ const Header = () => {
         </div>
 
         <div className='custom-header-right'>
-             <RainbowKitCustomConnectButton />
-            {/* <ConnectButton client={client} chains={[gnosisChain]} autoConnect= {true} connectButton={{
+             {/* <RainbowKitCustomConnectButton /> */}
+            <ConnectButton client={client} chains={[gnosisChain, liskSepolia, liskSepolia]} autoConnect= {true} connectButton={{
                  label: "Connect Wallet",
                  className: "my-custom-class",
                  style: {
@@ -76,7 +79,7 @@ const Header = () => {
                     fontFamily: "var(--font-anek-bangla)",
                  },
             }}
-             /> */}
+             />
             <FaucetButton /> 
 
              {connectedAddress &&  <div className='custom-header-avatar'>
